533839a3509449722cbfe4a5343893a716a43a22
SarikaMunurathi <sarikaammu458@gmail.com>
2024-05-29 13:27:51 +0530

    by sarika

---
+++ 01-hello-world.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "This is the sample program to start learning shell-script"
---
+++ 05-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Enter User Name :"
+read Username 
+echo "Enter Password :"
+read Password
+
+echo "User Name is:" $Username , "Password is:" $Password
