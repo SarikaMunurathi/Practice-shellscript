0ed707b85df65cbca8e327aaa32b6df23559aa6a
SarikaMunurathi <sarikaammu458@gmail.com>
2024-05-29 14:07:58 +0530

    by sarika

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -2,4 +2,7 @@
 
 Flowers=("rose","jasmine","lilly")
 
+echo "List of Flowers : ${Flowers[0]}"
+echo "List of Flowers : ${Flowers[1]}"
+echo "List of Flowers : ${Flowers[2]}"
 echo "List of Flowers : ${Flowers[@]}"
