879660cf5ea09ba118daedfa80fb41f12a515007
SarikaMunurathi <sarikaammu458@gmail.com>
2024-05-30 04:57:42 +0530

    by sarika

--- 08-specialvariables.sh
+++ 08-specialvariables.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-
+echo "All variables: $@"
 echo "No of parameters passed:" $#
 echo "Script name:" $0
 echo "Current Working Directory:" $PWD
 echo "Home directory of the current user:" $Home
 echo "Which user is running this script" $USER
-echo "All variables: $@"
+echo "Host Name: $HOSTNAME"
+echo "Process ID of the curresnt shell script: $$"
+sleep 20
+echo "Process ID for the last back ground command: $!"
