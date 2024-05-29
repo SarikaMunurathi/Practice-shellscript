bfeb2213feff10a28f8f50ca71b26c70f9bea8dd
SarikaMunurathi <sarikaammu458@gmail.com>
2024-05-30 04:52:30 +0530

    by sarika

--- 08-specialvariables.sh
+++ 08-specialvariables.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo "All variables: $@"
+
 echo "No of parameters passed:" $#
 echo "Script name:" $0
 echo "Current Working Directory:" $PWD
 echo "Home directory of the current user:" $Home
 echo "Which user is running this script" $USER
+echo "All variables: $@"
