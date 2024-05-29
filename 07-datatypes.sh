6263552f45d2bfa85f3292198aebaa82fe84cbca
SarikaMunurathi <sarikaammu458@gmail.com>
2024-05-30 04:36:07 +0530

    by sarika

--- 07-datatypes.sh
+++ 07-datatypes.sh
@@ -3,6 +3,6 @@
 NO1=$1
 NO2=$2
 
-echo "Enter First Number: ${NO1}"
-echo "Enter Second Number: ${NO2}"
-echo $(($NO1+$NO2))
+echo "Enter First Number: $NO1"
+echo "Enter Second Number: $NO2"
+echo SUM=$(($NO1+$NO2))
